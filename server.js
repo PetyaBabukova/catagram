3c61fc61ea8e11c960f6d4cdc199ee782dddc771
Petya Babukova <p_babukova@abv.bg>
2022-02-14 17:57:20 +0200

    Add static files

--- server.js
+++ server.js
@@ -1,28 +1,31 @@
 const express = require('express');
 
-const checkCatIdMiddleware = require('./middlewares/middleware')
+const checkCatIdMiddleware = require('./middlewares/middleware');
+const logger = require('./middlewares/loggerMiddleware');
 
 const app = express();
 
 const cats = [];
 
+// app.use('/static', express.static('public')); //На този раут ползвай мидълуера екпрес статик и папка публик e достъпна на този раут. Внимание - като се линква цсс-а, трябвада се слага или да не се срага /static в зависимост от това кой от двата метода ползваме. 7 и 8 ред в html файла.
+
+app.use(express.static('public')); //Всичко в папка пъблик е публично достъпно
+
+app.use(logger);
+
 app.get('/', (req, res) => {
-    console.log("Hello from console");
-    res.send('Hello world from express!')
+    res.send('index page')
+    // res.sendFile(__dirname + '/views/home.html') //тук трябва да се сложи абсолютен път!
+    // res.sendFile('./views/home.html', { root: __dirname}); // може и така Ве едното има точка в другото няма - в пътя!
 });
 
 //  app.get('/download', (req, res)=>{
 //      res.download('./views/home.html');
 //  });
 
-// app.get('/download', (req, res) => {
-//     res.attachment('./views/home.html');
-//     res.send('File has been send');
-// });
-
-app.get('/download', (req, res)=>{
-    // res.sendFile(__dirname + '/views/home.html') //тук трябва да се сложи абсолютен път!
-    res.sendFile('./views/home.html', { root: __dirname}); // може и така Ве едното ива точка в другото няма - в пътя!
+app.get('/download', (req, res) => {
+    res.attachment('./views/home.html');
+    res.end();
 });
 
 
